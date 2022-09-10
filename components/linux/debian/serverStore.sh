bb804d60d02f827c90fd4eb3933cfaa92f8b76f9
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-09-10 23:26:37 +0900

    wip

--- components/linux/debian/serverStore.sh
+++ components/linux/debian/serverStore.sh
@@ -4,8 +4,17 @@ jobName="Install server tools"
 echo "----------------------------"
 echo "⋆⸜ '$jobName' started ⸝⋆";
 echo "----------------------------"
+# ufw
+sudo apt install -y ufw 
+sudo ufw default deny
+sudo ufw allow 22
+sudo ufw enable
+
+sudo systemctl start ufw
+sudo systemctl enable ufw
+
 # nginx
-sudo apt install -y nginx
+sudo apt install -y nginx 
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
--- components/linux/debian/settings.sh
+++ components/linux/debian/settings.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -eu
 
+# user追加
+
+#タイムゾーン
+sudo timedatectl set-timezone 'Asia/Tokyo'
+
 # visudoでのエディターをvimに変更
 sudo update-alternatives --set editor /usr/bin/vim.basic
