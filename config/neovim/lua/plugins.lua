8ec448ac26516e04e518f06187b1302273512b8a
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-09-21 09:43:42 +0900

    wip

--- config/neovim/lua/plugins.lua
+++ config/neovim/lua/plugins.lua
@@ -4,6 +4,9 @@ require'packer'.startup(function()
     use "rebelot/kanagawa.nvim"
     use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
     use("williamboman/nvim-lsp-installer")
+
+    -- vimのide化
+    use {'neoclide/coc.nvim', branch = 'release'}
         -- filer
     -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
