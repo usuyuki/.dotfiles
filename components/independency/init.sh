c8ad135645fe023c28a4dfd5fed11f393eb4c5c5
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-04-02 10:34:53 +0900

    fix sudo抜けてる

--- components/independency/init.sh
+++ components/independency/init.sh
@@ -1,4 +1,4 @@
-sed -i 's@archive.ubuntu.com@ftp.jaist.ac.jp/pub/Linux@g' /etc/apt/sources.list
+sudo sed -i 's@archive.ubuntu.com@ftp.jaist.ac.jp/pub/Linux@g' /etc/apt/sources.list
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y autoremove
