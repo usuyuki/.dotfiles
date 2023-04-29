5bfef31ece4f43ece7a3aadb481dcda2b69c6828
usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-04-29 11:55:17 +0900

    fix gitconfig

--- components/independency/init.sh
+++ components/independency/init.sh
@@ -1,6 +1,5 @@
-sudo sed -i 's@archive.ubuntu.com@ftp.jaist.ac.jp/pub/Linux@g' /etc/apt/sources.list
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y autoremove
-sudo apt -y install build-essential git curl
+echo "うすゆきdotfilesをインストールします"
 git clone https://github.com/usuyuki/.dotfiles.git ~/.dotfiles
+
+# シンボリックリンク貼る
+sh ~/.dotfiles/components/linux/common/link.sh
--- setup/ubuntu_daily/setup.sh
+++ setup/ubuntu_daily/setup.sh
@@ -4,7 +4,7 @@ sh ~/.dotfiles/components/linux/debian/settings.sh
 # 必要なものをインストール
 sh ~/.dotfiles/components/linux/debian/getApplication/installer_daily.sh
 
-# シンボリックリンク貼る neovimの設定的にあとにしないと危険
+# シンボリックリンク貼る 
 sh ~/.dotfiles/components/linux/common/link.sh
 
 # 必要事項のメッセージ
