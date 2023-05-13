5e3650ae43cef6cf196136224e09962efa442942
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-05-13 13:45:40 +0900

    fix asdfのphp修正

--- components/linux/debian/getApplication/packages/asdf/lang/asdf_php.sh
+++ components/linux/debian/getApplication/packages/asdf/lang/asdf_php.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -eu
 echo "🧃 php 🧃"
-sudo apt -y install autoconf bison re2c libcurl4-openssl-dev libgd-dev
-asdf plugin add php https://github.com/asdf-community/asdf-php
-asdf install php latest
-asdf global php latest
-asdf-community/asdf-php
-composer global require friendsofphp/php-cs-fixer
-asdf reshim
-php-cs-fixer --version
+sudo apt -y install autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev
+asdf install php 8.2
+asdf global php 8.2
