f7b823b78f67d8f3ed270872780368615e52fc0e
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-06-17 09:30:39 +0900

    fix 動かないasdfphpを修正

--- components/linux/debian/getApplication/packages/asdf/lang/asdf_node.sh
+++ components/linux/debian/getApplication/packages/asdf/lang/asdf_node.sh
@@ -2,8 +2,8 @@
 echo "🧃 node 🧃"
 sudo apt -y install dirmngr gpg gawk
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs lts
-asdf global nodejs lts
+asdf install nodejs 18.16.0
+asdf global nodejs 18.16.0
 # yarnはnode経由が良さそう
 # この2つはシェルスクリプトで実行できないので後でやるように催促する
 # corepack enable
--- components/linux/debian/getApplication/packages/asdf/lang/asdf_php.sh
+++ components/linux/debian/getApplication/packages/asdf/lang/asdf_php.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 echo "🧃 php 🧃"
-sudo apt -y install autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev
-asdf install php 8.2
-asdf global php 8.2
+sudo apt -y install autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev autoconf
+asdf plugin-add php https://github.com/asdf-community/asdf-php.git
+asdf install php 8.2.7
+asdf global php 8.2.7
