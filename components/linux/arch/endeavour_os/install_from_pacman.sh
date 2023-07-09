634c639701ca3d20c72a3fe8523f60f56d5acd8c
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-07-09 13:41:42 +0900

    wip

--- components/linux/arch/endeavour_os/install_from_pacman.sh
+++ components/linux/arch/endeavour_os/install_from_pacman.sh
@@ -10,6 +10,13 @@ sudo pacman -Syyu
 sudo pacman -S fcitx-im fcitx-mozc
 
 # 種々の言語が依存しがちなので、先に言語のバージョン管理環境を整える
+# rtxがrust依存なのでrustだけは先に入れる
+sudo pacman -S rustup
+rustup default stable
+# rtxの導入(rustでビルドされるっぽい)
+(git clone https://aur.archlinux.org/rtx.git && cd rtx && makepkg -si)
+
+rtx install
 
 # 最初から入ってる:git,yay,
 sudo pacman -S code vim neofetch
--- components/linux/arch/settings.sh
+++ components/linux/arch/settings.sh
@@ -1,15 +1,17 @@
 # bash_aliasesを読み込めるように設定
+echo "bash_aliasesの読み込み設定"
 aliasLoad="
 # bash_aliases読み込み
 if [ -f ~/.bash_aliases ]; then                                                                                                                                                    
 . ~/.bash_aliases                                                                                                                                                          
 fi 
-"]
+"
 # ubuntuと違ってbash_aliases読み込みの項目がないのでbashrcに上書き
 echo "$aliasLoad" >>~/.bashrc
 
 # 日本語のためのパッケージのインストールはpacmanのインストールスクリプトで行う
 # xprofile作成
+echo "日本語環境のためのxprofileの設定"
 jaConfig="
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
@@ -17,3 +19,5 @@ export XMODIFIERS=@im=fcitx
 "
 
 echo "$jaConfig" >~/.xprofile
+
+echo "ログインし直すことで日本語入力環境が有効になります。"
