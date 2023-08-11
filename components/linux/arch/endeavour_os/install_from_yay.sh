e0beb466f92c94e87bb9e130a9ff46908e88ac1e
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-08-11 23:59:34 +0900

    feat keybasenado

--- components/linux/arch/endeavour_os/install_from_yay.sh
+++ components/linux/arch/endeavour_os/install_from_yay.sh
@@ -4,11 +4,12 @@ echo "⋆⸜ '$jobName' started ⸝⋆"
 echo "----------------------------"
 
 # braveの自前ビルドは4,5時間かけてもbinをインストール
-yay -S google-chrome slack-desktop brave-bin xfluxd postman-bin zoom figma-linux-bin visual-studio-code-bin
+yay -S google-chrome slack-desktop brave-bin xfluxd postman-bin zoom figma-linux-bin visual-studio-code-bin keybase-bin
 
 
 # 不安定
 # vscodeはpacmanでcodeを入れるとOSS版になるのだが、syncがうまく動かないのでMS版を入れる
+# keybaseはpacmanで入れるとGUI無いのでAUR版を入れる
 
 # 不要パッケージの削除
 sudo yay -Yc
