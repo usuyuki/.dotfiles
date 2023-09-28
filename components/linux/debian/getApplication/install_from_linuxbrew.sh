0bd061215213b54c53f676116ea41624b05f9f69
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-09-28 14:00:52 +0900

    wip

--- components/linux/debian/getApplication/install_from_linuxbrew.sh
+++ components/linux/debian/getApplication/install_from_linuxbrew.sh
@@ -3,9 +3,9 @@
 
 
 
-sh /home/fss3/usuyuki/.dotfiles/components/linux/debian/getApplication/packages/zellij.sh
+# sh /home/fss3/usuyuki/.dotfiles/components/linux/debian/getApplication/packages/zellij.sh
 # z
-sh /home/fss3/usuyuki/.dotfiles/components/linux/debian/getApplication/packages/z.sh
+# sh /home/fss3/usuyuki/.dotfiles/components/linux/debian/getApplication/packages/z.sh
 
 # brew系統
 brew install gtop
