e596d44e7fad5ac957e917124da31f4eae68d2f9
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-10-02 10:49:51 +0900

    wip

--- components/linux/arch/endeavour_os/install_esp-idf.sh
+++ components/linux/arch/endeavour_os/install_esp-idf.sh
@@ -1,4 +1,9 @@
-yay -S esp-idf
-sh /opt/esp-idf/install.sh
-# zshrcにパス追加
-# echo "source /opt/esp-idf/export.sh" >>~/.zshrc
+# yay -S esp-idf ←これだとうまくいかない
+# https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html で指定の依存関係
+sudo pacman -S --needed gcc git make flex bison gperf python cmake ninja ccache dfu-util libusb
+(mkdir -p ~/esp &&
+	cd ~/esp &&
+	git clone -b v5.1.1 --recursive https://github.com/espressif/esp-idf.git)
+(cd ~/esp/esp-idf &&
+	./install.sh all)
+# 使う時→　get_idf →　idf.py build →　idf.py -p /dev/tty flash
--- script/imagemagic-jpeg-quality.sh
+++ script/imagemagic-jpeg-quality.sh
@@ -6,13 +6,11 @@ mkdir -p compressed
 # 引数から圧縮率を取得（デフォルト値は75）
 compression_quality=${1:-75}
 
-# .JPG画像のみ処理
-for file in *.JPG; do
+for file in *.JPG *.jpg; do
     if [ -f "$file" ]; then
-        # 圧縮処理
-        output_file="compressed/${file%.JPG}_compressed.JPG"
+        output_file="compressed/${file%.*}_compressed.JPG"
         convert "$file" -quality $compression_quality "$output_file"
-        echo "圧縮完了: $file -> $output_file"
+        echo "${file##*.}圧縮完了: $file -> $output_file"
     fi
 done
 
