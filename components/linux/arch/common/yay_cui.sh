312e83bc5f2ca9ffc3782680597ca073b5984de6
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-10-19 11:23:04 +0900

    wip 背景変更、依存修正など

--- components/linux/arch/common/yay_cui.sh
+++ components/linux/arch/common/yay_cui.sh
@@ -7,9 +7,11 @@ echo "----------------------------"
 
 # optional
 yay -S tty-clock 
-
 # tty-clock i3wmでよく見るCUIの時計
 
+# platfomrio依存
+yay -S platformio
+sudo pip install Click semantic_version requests bottle colorama marshmallow pyelftools pyserial starlette tabulate uvicorn wsproto pyelftools 
 
 echo "˗ˋˏ '$jobName' success ˎˊ˗ "
 echo "■■■■■■■■■■oud
