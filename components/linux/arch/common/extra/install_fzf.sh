6d5fd49e1183335e9323b40583de3e76c2aa775a
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-10-21 07:52:02 +0900

    fix fzf

--- components/linux/arch/common/extra/install_fzf.sh
+++ components/linux/arch/common/extra/install_fzf.sh
@@ -1,7 +1,9 @@
 sudo pacman -S fzf bat
 # インストールコマンドを叩く
-echo 'export FZF_CTRL_T_COMMAND="rg --files --hidden --follow --glob \"!.git/*\""' >> ~/.zshrc
-echo 'export FZF_CTRL_T_OPTS="--preview \"bat  --color=always --style=header,grid --line-range :100 {}\""' >> ~/.zshrc
+echo 'source /usr/share/fzf/key-bindings.zsh' >>~/.zshrc
+echo 'source /usr/share/fzf/completion.zsh' >>~/.zshrc
+# echo 'export FZF_CTRL_T_COMMAND="rg --files --hidden --follow --glob \"!.git/*\""' >> ~/.zshrc
+# echo 'export FZF_CTRL_T_OPTS="--preview \"bat  --color=always --style=header,grid --line-range :100 {}\""' >> ~/.zshrc
 
 # コマンドについて
 echo 'alt+c でディレクトリ検索+移動'
