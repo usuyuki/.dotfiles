83519fa27ed8b699bf253aa5c10c1b399ca3c2df
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-11-28 19:20:18 +0900

    fix fzfでできることをしてなかった

--- components/linux/arch/common/extra/install_fzf.sh
+++ components/linux/arch/common/extra/install_fzf.sh
@@ -2,6 +2,8 @@ sudo pacman -S fzf bat
 # インストールコマンドを叩く
 echo 'source /usr/share/fzf/key-bindings.zsh' >>~/.zshrc
 echo 'source /usr/share/fzf/completion.zsh' >>~/.zshrc
+echo 'export FZF_CTRL_T_OPTS="--preview '\''bat -n --color=always {}'\'' --bind '\''ctrl-/:change-preview-window(down|hidden|)'\''"' >> ~/.zshrc
+echo 'export FZF_ALT_C_OPTS="--preview '\''tree -C {}'\''"' >> ~/.zshrc
 # echo 'export FZF_CTRL_T_COMMAND="rg --files --hidden --follow --glob \"!.git/*\""' >> ~/.zshrc
 # echo 'export FZF_CTRL_T_OPTS="--preview \"bat  --color=always --style=header,grid --line-range :100 {}\""' >> ~/.zshrc
 
