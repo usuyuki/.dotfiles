e5423a2c0ebb0ded21ec991250beaaa90be97a98
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-19 21:01:56 +0900

    feat docker-cli arch linux

---
+++ components/linux/arch/common/extra/install_docker-cli.sh
@@ -0,0 +1,11 @@
+sudo pacman -S docker docker-compose
+sudo gpasswd -a $USER docker
+# sudo systemctl start docker
+# sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl restart docker
+# error getting credentials - err: exec: "docker-credential-desktop": executable file not found in $PATH,
+# が出た場合
+# https://stackoverflow.com/questions/65896681/exec-docker-credential-desktop-exe-executable-file-not-found-in-path
+# sudo vi ~/.docker/config.json でcredsStoreをcredStoreにすると解決する
