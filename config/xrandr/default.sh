5f038cec6dd12154d4bea5ae896c4cee34a59a91
usuyuki <63891531+usuyuki@users.noreply.github.com>
2024-07-01 23:20:10 +0900

    wip

--- config/xrandr/default.sh
+++ config/xrandr/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --off --output HDMI-A-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+xrandr --output eDP --off --output HDMI-A-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
